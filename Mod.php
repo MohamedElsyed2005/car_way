c2e76b4a2d8e076a448561f32209aa5a08240c03
mohamed <2305273@anu.edu.eg>
2024-12-07 01:24:13 +0200

    ADD STATUS

--- Mod.php
+++ Mod.php
@@ -10,6 +10,7 @@ if(isset($_POST["submit"])){
     $plate_id = $_POST["plate_id"];
     $type = $_POST["type"];
     $year = $_POST["year"];
+    $status = $_POST["Status"];
 
 
 
@@ -21,7 +22,7 @@ if(isset($_POST["submit"])){
     }
     else{
       //  car_id	plate_id	brand	model	type	manufacture	year	color	office_id	status	
-        $query = "INSERT INTO car VALUES( ''  , '$plate_id' , '$brand' , '$model', '$type' , '$manufacture', '$year', '$color' ,  '$office_id' , '' )";
+        $query = "INSERT INTO car VALUES( ''  , '$plate_id' , '$brand' , '$model', '$type' , '$manufacture', '$year', '$color' ,  '$office_id' , '$status' )";
         
         mysqli_query($conn, $query);
 
