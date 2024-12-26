c7e17c1eba1670a3d0dd3c9186c4006b69f8b612
Muhammad221B <168672825+Muhammad221B@users.noreply.github.com>
2024-12-26 23:53:52 +0200

    Admin_Login_Without_Radio

--- admin/loginAdmin.php
+++ admin/loginAdmin.php
@@ -1,15 +1,18 @@
 <?php
 session_start();
 require '../config.php';
+$office_id = $_SESSION["id"];
+$sql =         "SELECT *
+                FROM office_accounts
+                WHERE office_id = '$office_id';";
 
 if(!empty($_SESSION["id"])) {
     header("Location: dashboard.php");
 }
 if(isset($_POST["submit"])){
-    $office_id = $_POST["btn"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-    $result = mysqli_query($conn, "SELECT * FROM office_accounts WHERE office_id = '$office_id' AND Email = '$email'");
+    $result = mysqli_query($conn, "SELECT * FROM office_accounts WHERE Email = '$email'");
     $row = mysqli_fetch_assoc($result);
     if (mysqli_num_rows($result) > 0) {
         if ($password == $row["password"]) {
